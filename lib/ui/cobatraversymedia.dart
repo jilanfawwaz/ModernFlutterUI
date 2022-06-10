ac80dbd435e3168992486f5e95b185480d126fe8
M. Jilan Fawwaz <47166420+jilanfawwaz@users.noreply.github.com>
2022-06-11 07:49:03 +0800

    coba traversy done versy diri sendiri

--- lib/ui/cobatraversymedia.dart
+++ lib/ui/cobatraversymedia.dart
@@ -13,11 +13,57 @@ class _CobaTraversyState extends State<CobaTraversy> {
 
   Set<WordPair> savedWord = {};
 
+  Widget favoritePage() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Favorite Word"),
+      ),
+      // body: savedWord.isEmpty
+      //     ? Center(child: Text("You didn't like anything"))
+      //     : ListView(
+      //         children: savedWord.map(
+      //           (WordPair e) {
+      //             //children: savedWord.map((e) {
+      //             return Column(
+      //               children: [
+      //                 ListTile(
+      //                   title: Text(e.asPascalCase.toString()),
+      //                 ),
+      //                 Divider(),
+      //               ],
+      //             );
+      //           },
+      //         ).toList(),
+      //       ),
+
+      body: ListView.builder(
+        itemCount: savedWord.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(savedWord.toList()[index].toString()),
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Coba Word Pair"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => favoritePage(),
+                  ),
+                );
+              },
+              icon: Icon(Icons.list))
+        ],
       ),
       body: ListView.builder(
         itemBuilder: (context, index) {
@@ -31,7 +77,7 @@ class _CobaTraversyState extends State<CobaTraversy> {
           }
           return ListTile(
             title: Text(
-              wordPair[word].toString(),
+              wordPair[word].asPascalCase.toString(),
             ),
             trailing: IconButton(
               onPressed: () {
