978850c54bcf931d5a308cff62276f8afe9eac01
M. Jilan Fawwaz <47166420+jilanfawwaz@users.noreply.github.com>
2022-06-19 15:21:56 +0800

    mediaquery, constraint, flexible, expanded done

--- lib/ui/cobamediaquery.dart
+++ lib/ui/cobamediaquery.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+//import 'package:flutter/services.dart';
 
 class CobaMediaQuery extends StatelessWidget {
   const CobaMediaQuery({Key? key}) : super(key: key);
@@ -25,6 +25,178 @@ class CobaMediaQuery extends StatelessWidget {
 
     return Scaffold(
       appBar: _appBar,
+      /*body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              color: Colors.blue,
+              width: _width * 0.3,
+              height: _bodyHeight * 0.8,
+              //NOTE: Layoutbuilder
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        //constraints: BoxConstraints(maxWidth: 40),
+                        width: constraints.maxWidth * 0.3,
+                        height: constraints.maxHeight * 0.9,
+                        color: Colors.amber,
+                      ),
+                      Container(
+                        //constraints: BoxConstraints(maxWidth: 40),
+                        width: constraints.maxWidth * 0.3,
+                        height: constraints.maxHeight * 0.9,
+                        color: Colors.amber,
+                      ),
+                      Container(
+                        //constraints: BoxConstraints(maxWidth: 40),
+                        width: constraints.maxWidth * 0.3,
+                        height: constraints.maxHeight * 0.9,
+                        color: Colors.amber,
+                      ),
+                      // LayoutBuilder(
+                      //   builder: (context, constraints) {
+                      //     return Container(
+                      //       width: _width * 0.02,
+                      //       height: constraints.maxHeight * 0.4,
+                      //       color: Colors.amber,
+                      //     );
+                      //   },
+                      // ),
+                    ],
+                  );
+                },
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              width: _width * 0.3,
+              height: _bodyHeight * 0.8,
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        //constraints: BoxConstraints(maxWidth: 40),
+                        width: constraints.maxWidth * 0.3,
+                        height: constraints.maxHeight * 0.9,
+                        color: Colors.amber,
+                      ),
+                      Container(
+                        //constraints: BoxConstraints(maxWidth: 40),
+                        width: constraints.maxWidth * 0.3,
+                        height: constraints.maxHeight * 0.9,
+                        color: Colors.amber,
+                      ),
+                      Container(
+                        //constraints: BoxConstraints(maxWidth: 40),
+                        width: constraints.maxWidth * 0.3,
+                        height: constraints.maxHeight * 0.9,
+                        color: Colors.amber,
+                      ),
+                      // LayoutBuilder(
+                      //   builder: (context, constraints) {
+                      //     return Container(
+                      //       width: _width * 0.02,
+                      //       height: constraints.maxHeight * 0.4,
+                      //       color: Colors.amber,
+                      //     );
+                      //   },
+                      // ),
+                    ],
+                  );
+                },
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              width: _width * 0.3,
+              height: _bodyHeight * 0.8,
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        //constraints: BoxConstraints(maxWidth: 40),
+                        width: constraints.maxWidth * 0.3,
+                        height: constraints.maxHeight * 0.9,
+                        color: Colors.amber,
+                      ),
+                      Container(
+                        //constraints: BoxConstraints(maxWidth: 40),
+                        width: constraints.maxWidth * 0.3,
+                        height: constraints.maxHeight * 0.9,
+                        color: Colors.amber,
+                      ),
+                      Container(
+                        //constraints: BoxConstraints(maxWidth: 40),
+                        width: constraints.maxWidth * 0.3,
+                        height: constraints.maxHeight * 0.9,
+                        color: Colors.amber,
+                      ),
+                      // LayoutBuilder(
+                      //   builder: (context, constraints) {
+                      //     return Container(
+                      //       width: _width * 0.02,
+                      //       height: constraints.maxHeight * 0.4,
+                      //       color: Colors.amber,
+                      //     );
+                      //   },
+                      // ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),*/
+
+      /*body: Center(
+        child: Container(
+          width: _width * 0.9,
+          height: _bodyHeight * 0.3,
+          color: Colors.blue,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  return Container(
+                    width: _width * 0.25,
+                    height: constraints.maxHeight * 0.5,
+                    color: Colors.amber,
+                  );
+                },
+              ),
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  return Container(
+                    width: _width * 0.25,
+                    height: constraints.maxHeight * 0.5,
+                    color: Colors.amber,
+                  );
+                },
+              ),
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  return Container(
+                    width: _width * 0.25,
+                    height: constraints.maxHeight * 0.5,
+                    color: Colors.amber,
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),*/
+
       body: Center(
         child: _isLandscape
             ? Row(
@@ -34,49 +206,218 @@ class CobaMediaQuery extends StatelessWidget {
                     height: _bodyHeight * 0.3,
                     width: _width * 0.4,
                     color: Colors.blue,
-                    child: Column(
-                      children: [
-                        Container(
-                          width: 50,
-                          height: 50,
-                          color: Colors.amber,
-                        )
-                      ],
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              width: constraints.maxWidth * 0.3,
+                              height: constraints.maxHeight * 0.8,
+                              color: Colors.amber,
+                            ),
+                            Container(
+                              width: constraints.maxWidth * 0.3,
+                              height: constraints.maxHeight * 0.8,
+                              color: Colors.amber,
+                            ),
+                            Container(
+                              width: constraints.maxWidth * 0.3,
+                              height: constraints.maxHeight * 0.8,
+                              color: Colors.amber,
+                            ),
+                          ],
+                        );
+                      },
                     ),
                   ),
                   Container(
                     height: _bodyHeight * 0.3,
                     width: _width * 0.4,
                     color: Colors.blue,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              width: constraints.maxWidth * 0.3,
+                              height: constraints.maxHeight * 0.8,
+                              color: Colors.amber,
+                            ),
+                            Container(
+                              width: constraints.maxWidth * 0.3,
+                              height: constraints.maxHeight * 0.8,
+                              color: Colors.amber,
+                            ),
+                            Container(
+                              width: constraints.maxWidth * 0.3,
+                              height: constraints.maxHeight * 0.8,
+                              color: Colors.amber,
+                            ),
+                          ],
+                        );
+                      },
+                    ),
                   ),
                 ],
               )
             : Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Container(
-                    height: _bodyHeight * 0.4,
+                    height: _bodyHeight * 0.2,
                     width: _width * 0.3,
                     color: Colors.blue,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        LayoutBuilder(
-                          builder: (context, constraints) {
-                            return Container(
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
                               width: constraints.maxWidth * 0.8,
-                              height: 100,
+                              //width: 100,
+                              height: constraints.maxHeight * 0.3,
                               color: Colors.amber,
-                            );
-                          },
+                            ),
+                            Container(
+                              width: constraints.maxWidth * 0.8,
+                              //width: 100,
+                              height: constraints.maxHeight * 0.3,
+                              color: Colors.amber,
+                            ),
+                            Container(
+                              width: constraints.maxWidth * 0.8,
+                              //width: 100,
+                              height: constraints.maxHeight * 0.3,
+                              color: Colors.amber,
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    height: _bodyHeight * 0.2,
+                    width: _width * 0.3,
+                    color: Colors.blue,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              width: constraints.maxWidth * 0.8,
+                              //width: 100,
+                              height: constraints.maxHeight * 0.3,
+                              color: Colors.amber,
+                            ),
+                            Container(
+                              width: constraints.maxWidth * 0.8,
+                              //width: 100,
+                              height: constraints.maxHeight * 0.3,
+                              color: Colors.amber,
+                            ),
+                            Container(
+                              width: constraints.maxWidth * 0.8,
+                              //width: 100,
+                              height: constraints.maxHeight * 0.3,
+                              color: Colors.amber,
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: _width * 0.9,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Flexible(
+                          fit: FlexFit.tight,
+                          flex: 5,
+                          child: Container(
+                            height: 20,
+                            color: Colors.blue,
+                            child: Text("Haloo 1"),
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          fit: FlexFit.tight,
+                          child: SizedBox(),
+                        ),
+                        Flexible(
+                          //NOTE:Flexfit Flexible (- ketika dikasih flexible dan flexfit, ukuran container akan memenuhi ukuran row )
+                          fit: FlexFit.tight,
+                          //fit: FlexFit.loose,
+                          flex: 15,
+                          child: Container(
+                            //width: 40, //kalau di row make flexible dan ada fit: , ukuran width akan diabaikan
+                            height: 20,
+                            color: Colors.red,
+                            child: Text("Haloo 2"),
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          fit: FlexFit.tight,
+                          child: SizedBox(),
+                        ),
+                        Flexible(
+                          fit: FlexFit.tight,
+                          //fit: FlexFit.loose,
+                          flex: 5,
+                          child: Container(
+                            height: 20,
+                            color: Colors.amber,
+                            child: Text("Haloo 3"),
+                          ),
                         ),
                       ],
                     ),
                   ),
-                  Container(
-                    height: _bodyHeight * 0.4,
-                    width: _width * 0.3,
-                    color: Colors.blue,
+                  Row(
+                    //mainAxisAlignment: MainAxisAlignment.center, //kalau dikasih mainaxisalignment.center, space putih di kiri kana tetap punya simerah
+                    children: [
+                      Flexible(
+                        //flexible bilang: hey container kamu isi satu layar, tapi kalau  ada container lain yang ditemenin ama flexible juga, lu bagi sama rata yaa jatah space nya... kalo containernya gaditemenin ama flexible ya lu kasih jatah sesuai width dia aja
+                        fit: FlexFit
+                            .tight, //jatah space gue gue mau isi sama warna gue, berati si width kacangin aja
+                        child: Container(
+                          height: 20,
+                          width: 20, //karena ada flexfit.tight width dikacangin
+                          color: Colors.blue,
+                        ),
+                      ),
+                      Flexible(
+                        //nih gue flexible juga, tapi
+                        fit: FlexFit
+                            .loose, //(- default fit dari flexible, gaditulis sebenarnya gapapa) jatah space gue mau putih aja ah, lebar warnanya sesuai width aja, jatah putihnya kalau ada container lain ditaroh dibelakang aja
+                        child: Container(
+                          height: 20,
+                          width: 20,
+                          color: Colors.red,
+                        ),
+                      ),
+                      Flexible(
+                        fit: FlexFit.tight,
+                        child: Container(
+                          height: 20,
+                          //width: 20, //flexible kalau gadikasih width, maka secara default akan dijadikan fit: flexfit.tight
+                          color: Colors.amber,
+                        ),
+                      ),
+                      //Flexible(fit: flexfit.tight) bisa ditulis lebih pendek menjadi expanded()
+                      Expanded(
+                        child: Container(
+                          height: 20,
+                          color: Colors.green,
+                        ),
+                      ),
+                      //space kosong di paling belakang ini adalah jatah space punya si merah
+                    ],
                   ),
                 ],
               ),
